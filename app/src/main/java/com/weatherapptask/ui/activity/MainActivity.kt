3e4978e2a3860f4e112b52ad3ef9e443996b27f6
EslamMohamed3396 <em0109224@gmail.com>
2022-09-16 14:05:34 +0200

    app structure

--- app/src/main/java/com/weatherapptask/ui/activity/MainActivity.kt
+++ app/src/main/java/com/weatherapptask/ui/activity/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.weatherapptask.ui.activity
 
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.NavController
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.weatherapptask.R
 import com.weatherapptask.databinding.ActivityMainBinding
 import com.weatherapptask.ui.base.BaseActivity
@@ -20,46 +15,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun onCreateInit() {
-        initNavigationBottom()
+        navController()
     }
 
-    private fun initNavigationBottom() {
-        binding.bottomNavigation.setupWithNavController(getNavController())
-        binding.bottomNavigation.itemIconTintList = null
-        binding.bottomNavigation.setOnItemSelectedListener { item ->
-            return@setOnItemSelectedListener onNavItemDestinationSelected(
-                item,
-                getNavController()
-            )
-        }
-    }
-
-    private fun onNavItemDestinationSelected(
-        item: MenuItem,
-        navController: NavController
-    ): Boolean {
-
-        val builder = NavOptions.Builder()
-            .setLaunchSingleTop(true)
-        if (item.order and Menu.CATEGORY_SECONDARY == 0) {
-            builder.setPopUpTo(
-                R.id.newsFragment,
-                false
-            )
-        }
-        val options = builder.build()
-        return try {
-            navController.navigate(item.itemId, null, options)
-            true
-        } catch (e: IllegalArgumentException) {
-            false
-        }
-    }
-
-
-    private fun getNavController(): NavController {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        return navHostFragment.navController
+    private fun navController() {
+        supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+            ?: return
     }
 }
